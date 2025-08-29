9dc77ed1916f90113d7f049d708b4a909ad50da4
vamshichauhan2 <vamshichauhan2@gmail.com>
2025-08-29 21:43:17 +0530

    Update index.jsx

--- frontend/src/components/admin/components/Resource/AddResource/index.jsx
+++ frontend/src/components/admin/components/Resource/AddResource/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 const API_BASE = import.meta.env.MODE === 'production'
-  ? "https://eynatranitandhra.onrender.com"
+  ? "https://e-yantra-backend.onrender.com"
   : import.meta.env.VITE_API_URL;
 
 
