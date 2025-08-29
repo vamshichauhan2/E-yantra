7d3502e4ecb8afa2f842b21b91d899ae88f8d280
vamshichauhan2 <vamshichauhan2@gmail.com>
2025-08-29 21:36:35 +0530

    Update ChangePassword1.jsx

--- frontend/src/components/admin/Auth/ChangePassword1/ChangePassword1.jsx
+++ frontend/src/components/admin/Auth/ChangePassword1/ChangePassword1.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 const API_BASE = import.meta.env.MODE === 'production'
-  ? "https://eynatranitandhra.onrender.com"
+  ? "https://e-yantra-backend.onrender.com"
   : import.meta.env.VITE_API_URL;
 
 
